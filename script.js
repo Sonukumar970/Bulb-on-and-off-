d165440fbd4420e941f97ba9f6ab0674e83afe4d
Sonukumar970 <120566533+Sonukumar970@users.noreply.github.com>
2024-07-18 12:12:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+document.getElementById("img").src="./img/of.png";
+document.getElementById("text").innerHTML="Off"
+document.getElementById("of").style.backgroundColor="rgb(245, 188, 0)"
+document.getElementById("of").style.color="white"
+
+
+
+function btnOn(){
+    document.getElementById("img").src="./img/on.png";
+    document.getElementById("text").innerHTML="On"
+    document.getElementById("on").style.backgroundColor="rgb(245, 188, 0)"
+    document.getElementById("on").style.color="white"
+    document.getElementById("of").style.backgroundColor="white"
+    document.getElementById("of").style.color="black"
+}
+
+
+function btnOf(){
+    document.getElementById("img").src="./img/of.png";
+    document.getElementById("text").innerHTML="Off"
+    document.getElementById("of").style.backgroundColor="rgb(245, 188, 0)"
+    document.getElementById("of").style.color="white"
+    document.getElementById("on").style.backgroundColor="white"
+    document.getElementById("on").style.color="black"
+}
+
+
+
